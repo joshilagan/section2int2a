07af98a35ec2de49a766b163933a6923b28d524c
joshi <joseph.ilagan.99@gmail.com>
2023-12-01 21:39:48 +0800

    .

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 const artistName = document.querySelector('.artist').textContent;
 console.log('Artist Name:', artistName);
 const lyriks = document.getElementById('lyrics')
+const tab = document.querySelector('.tab')
+
+//assign content div and other-albums div to variable
+const contents = document.querySelector('.content');
+const otherAlbs = document.querySelector('.other-albums');
+const lyricTab = document.querySelector('#lyric-tab');
 
 const API_KEY = '28e6d09161msh447eec6e356c7d9p1f9b7djsnd2f167725cca';
 const URL_SEARCH = 'https://genius-song-lyrics1.p.rapidapi.com/search/';
@@ -11,60 +17,11 @@ const header = {
   'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
 }
 
-//use artist name variable in the params query
-//use the API key variable in the RapidAPI Key header
-const geniusArtist = {
-  method: 'GET',
-  url: URL_SEARCH,
-  params: {
-    q: artistName,
-    per_page: '10',
-    page: '1'
-  },
-  headers: header
-};
-
-//Display the lyrics and song name
-const importLyrics = async () => {
-  //switch to lyrics tab
-  lyricTab.setAttribute('class', 'other-albums current space pointer');
-  otherAlbs.setAttribute('class', 'space pointer');
-
-  contents.innerHTML = 
-  `<div id="lyrics" class="current">   
-  </div>`
-
-  //Fetched SongID
-  const geniusArtistID = await axios.request(geniusArtist);
-  console.log('Song ID:', geniusArtistID.data.hits[0].result.id);
-  const songID = geniusArtistID.data.hits[0].result.id;
-
-
-  //Get Lyrics API and use the Song ID to params
-  const geniusLyrics = {
-    method: 'GET',
-    url: URL_LYRICS,
-    params: {id: songID},
-    headers: header
-    };
-
-  //Fetch Song name and update the DOM
-    console.log('Song Name:', geniusArtistID.data.hits[0].result.title);
-    document.querySelector('.song-name').textContent = geniusArtistID.data.hits[0].result.title;
-
-  //Fetch lyrics and update the DOM
-    const res = await axios.request(geniusLyrics);
-    document.getElementById('lyrics').innerHTML= res.data.lyrics.lyrics.body.html;
-    // console.log('Artist Song:', res.data.lyrics.lyrics.body.html);
-  }
-
-  
-  //==================START OF ALBUMS=============================
-  const SPOTIFY_URL_SEARCH = 'https://spotify23.p.rapidapi.com/search/';
-  const s_header = {
-    'X-RapidAPI-Key': API_KEY,
-    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-  }
+const SPOTIFY_URL_SEARCH = 'https://spotify23.p.rapidapi.com/search/';
+const s_header = {
+  'X-RapidAPI-Key': API_KEY,
+  'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+}
 
 const spotify = {
   method: 'GET',
@@ -79,21 +36,76 @@ const spotify = {
   headers: s_header
 };
 
+// use artist name variable in the params query
+// use the API key variable in the RapidAPI Key header
+const geniusArtist = {
+  method: 'GET',
+  url: URL_SEARCH,
+  params: {
+    q: artistName,
+    per_page: '10',
+    page: '1'
+  },
+  headers: header
+};
 
-// assign content div and other-albums div to variable
-const otherAlbs = document.querySelector('.other-albums');
-const lyricTab = document.querySelector('#lyric-tab');
-const contents = document.querySelector('.content')
+//Display the lyrics and song name
+const importLyrics = async () => {
+try {
+//switch to lyrics tab
+lyricTab.setAttribute('class', 'other-albums current space pointer');
+otherAlbs.setAttribute('class', 'space pointer');
+relatedArtistsTab.setAttribute('class', 'space pointer');
 
+contents.innerHTML = 
+`<div id="lyrics" class="current">   
+</div>`
+
+  //Fetched SongID
+  const geniusArtistID = await axios.request(geniusArtist);
+  // console.log('Song ID:', geniusArtistID.data.hits[0].result.id);
+  const songID = geniusArtistID.data.hits[0].result.id;
+
+
+  //Get Lyrics API and use the Song ID to params
+  const geniusLyrics = {
+    method: 'GET',
+    url: URL_LYRICS,
+    params: {id: songID},
+    headers: header
+    };
+  
+  //Fetch Song name and update the DOM
+  // console.log('Song Name:', geniusArtistID.data.hits[0].result.title);
+  const songNim = geniusArtistID.data.hits[0].result.title
+  document.querySelector('.song-name').textContent = songNim;
+  
+  //Fetch lyrics and update the DOM
+  const res = await axios.request(geniusLyrics);
+ 
+  const lyricx = res.data.lyrics.lyrics.body.html
+
+  document.getElementById('lyrics').innerHTML= lyricx;
+  
+}catch (error) {
+	console.log(error);
+}
+}
+
+//===============FOR OTHER ALBUMS===========================
 
 //add other-albums div and remove lyrics div
 const importAlbums = async () => {
-  contents.style.width = '390px'
-  contents.style.height = '400px'
-  
+  try{
+    contents.style.width = '390px'
+    contents.style.height = '400px'
+    tab.style.width = '500px'
+
   //update DOM for tab highlighter
   otherAlbs.setAttribute('class', 'other-albums current space pointer');
   lyricTab.setAttribute('class', 'space pointer');
+  relatedArtistsTab.setAttribute('class', 'space pointer');
+
   //change the div inside the contents div
   contents.innerHTML = 
   `<div id="other-albums-div">   
@@ -101,43 +113,44 @@ const importAlbums = async () => {
 
   const dive = document.querySelector('#other-albums-div')
   
-  // create image, title and year inside album-card
-  const response = await axios.request(spotify); 
+    // create image, title and year inside album-card
+    const response = await axios.request(spotify);          
+
+
+    // fetch artist ID
+    const artID = response.data.artists.items[0].data.uri;
+
+    // const artistID = artID.slice(15);
   
-  // fetch artist ID
-  const artID = response.data.artists.items[0].data.uri;
-  // console.log(artID);
-  const artistID = artID.slice(15);
-  // console.log(artistID);
-  
-  //loop thru the API using .map to create cards
-  response.data.albums.items.map((item) =>{
-    
-    //create cards
-    const diver = document.createElement('div');
-    diver.id = 'album-card';        
+    //loop thru the API using .map to create cards
+    response.data.albums.items.map((item) =>{
+
+        //create cards
+        const diver = document.createElement('div');
+        diver.id = 'album-card';        
         dive.appendChild(diver);
-        
-        // //complete the API Path and assign to variable
+
+        //complete the API Path and assign to variable
         const albumImg = item.data.coverArt.sources[0].url;
         const year = item.data.date.year;
         const title = item.data.name;
         
-        // //assign the API variables respectively
+        //assign the API variables respectively
         const img = document.createElement('img');
         img.id = 'card-pic';
         img.src = albumImg;
         img.alt = 'album-pic';
-        
+        img.setAttribute('class', `albumSelect ${item.data.uri}`)
+
         //create an a tag then append it to album-card div
         const aTag = document.createElement('a');
-        aTag.href = `https://open.spotify.com/artist/${artistID}`
-        aTag.target = '_blank';
+        // aTag.href = `https://open.spotify.com/artist/${artistID}`
+        // aTag.target = '_blank';
         aTag.setAttribute('class', 'pointer');
         diver.appendChild(aTag);
         //append the 'img tag' to the 'a tag'
         aTag.appendChild(img);
-        
+
         //create a 'p tag' for the title and append it to the album-card div
         const albName = document.createElement('p');
         albName.id = 'title';
@@ -149,21 +162,122 @@ const importAlbums = async () => {
         yr.id = 'year';
         yr.textContent = year;
         diver.appendChild(yr);        
-      }
-      )    
+    }
+    )
+    const albumDiv = document.querySelector('#other-albums-div')
+   
+    function albumClick(e){
+      
+      const slicedAlbumID = e.target.classList.item(1).slice(14);
+      getAlbumId(slicedAlbumID)
+    }
+
+   albumDiv.addEventListener('click', albumClick)
+
+
+  }  catch (error) {
+    console.log(error);
+    }
+    return 
     
-  return 
 }
-//update the DOM upon clicking Other Albums
-//import lyrics function displays song name and lyrics on load
+
+//========FOR RELATED ARTISTS================================
+
+//assign related artists Tab to a variable
+const relatedArtistsTab = document.querySelector('.related-artists');
+
+//Get related artists
+const importRelatedArtists = async () => {
+  
+  try {
+    contents.style.width = '390px'
+    contents.style.height = '400px'
+    tab.style.width = '500px'
+   
+      //update DOM for tab highlighter
+      relatedArtistsTab.setAttribute('class', 'related-artists current space pointer');
+      otherAlbs.setAttribute('class', 'other-albums space pointer');
+      lyricTab.setAttribute('class', 'space pointer');
+      
+        contents.innerHTML = 
+        `<div id="related-artists-div">   
+        </div>`
+        
+        const relArtDive = document.querySelector('#related-artists-div')
+        
+        const response = await axios.request(spotify);          
+        
+        //fetch artist ID
+        const artID = response.data.artists.items[0].data.uri;
+        const artistID = artID.slice(15);
+        
+        const relartist = {
+          method: 'GET',
+          url: 'https://spotify23.p.rapidapi.com/artist_related/',
+          params: {
+            id: artistID
+          },
+          headers: {
+            'X-RapidAPI-Key': '28e6d09161msh447eec6e356c7d9p1f9b7djsnd2f167725cca',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+          }
+  };
+  
+  //Fetch related artists image and name
+  const infow = await axios.request(relartist);
+  
+  //loop thru the API using .map to create cards
+  infow.data.artists.map((infows) =>{
+    
+    //create cards
+    const relArtCardDiv = document.createElement('div');
+    relArtCardDiv.id = 'rel-art-card';        
+    relArtDive.appendChild(relArtCardDiv);
+    
+    //Fetched Image, Artist Name and Artist ID and assign it to variables
+    const relArtImage = infows.images[0].url;
+    const relArtNim = infows.name;
+    const relArtId = infows.uri;
+    const relatedArtID = relArtId.slice(15);
+    
+    //assign the API variables respectively
+    const relArtImg = document.createElement('img');
+            relArtImg.id = 'related-artist-pic';
+            relArtImg.src = relArtImage;
+            relArtImg.alt = 'related-artist-pic';
+            
+            //create an a tag then append it to rel-art-card div
+            const relArtATag = document.createElement('a');
+            relArtATag.href = `https://open.spotify.com/artist/${relatedArtID}`;
+            relArtATag.target = '_blank';
+            relArtATag.setAttribute('class', 'pointer');
+            relArtCardDiv.appendChild(relArtATag);
+            //append the 'img tag' to the 'a tag'
+            relArtATag.appendChild(relArtImg);
+            
+            //create a 'p tag' for the title and append it to the rel-art-card div
+            const relArtName = document.createElement('p');
+            relArtName.id = 'art-name';
+            relArtName.textContent = relArtNim;
+            relArtCardDiv.appendChild(relArtName);
+          }
+          )   
+
+        }catch (error) {
+          console.log(error);
+        }
+        return
+}
+
+
 function init(){
   document.addEventListener('DOMContentLoaded', importLyrics);
   otherAlbs.addEventListener('click', importAlbums);
   lyricTab.addEventListener('click', importLyrics)
-
+  relatedArtistsTab.addEventListener('click', importRelatedArtists);
 }
 init()
-
 
 
 
